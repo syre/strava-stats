0c1d4e078dbadf5ce767c14a01c03437ecf52976
Søren Howe Gersager <thesyre@pm.me>
2025-07-31 12:45:38 +0200

    simplify and prettify layout, move distance by date to top

--- main.py
+++ main.py
@@ -24,75 +24,68 @@ pio.templates["reds"] = load_reds_template()
 pio.templates.default = "reds"
 
 app = Dash(external_scripts=["https://unpkg.com/@tailwindcss/browser@4"])
-app.title = "Strava Stats"
+app.title = "Strava Stats 🚲"
 
 
 app.layout = html.Div(children=[
-    html.H1(children='Strava Stats', className="text-3xl font-bold justify-self-center text-center w-100 mx-auto my-8 text-slate-700"),
+    html.H1(
+        "Strava Stats 🚲",
+        className="text-3xl font-bold text-center w-full mx-auto my-8 text-slate-700"
+    ),
     html.Div(
         children=[
-            html.Div(
-                children=[
-                    html.Div(children="Distance", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f'{"%.2f"%(calculate_total_distance(activities))} KM', className="text-sm text-gray-700"),
-                ],
-                className="flex-1 p-4"
-            ),
-            html.Div(
-                children=[
-                    html.Div(children="Streak ⚡", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f"{calculate_streak(activities)} days", className="text-sm text-gray-700"),
-                ],
-                className="flex-1 p-4"
-            ),
-            html.Div(
-                children=[
-                    html.Div(children="Rides", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f'{calculate_num_rides(activities)}', className="text-sm text-gray-700")
-                ],
-                className="flex-1 p-4"
-            ),
-            html.Div(
-                children=[
-                    html.Div(children="Duration", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f'{"%.2f"%(calculate_moving_time(activities)/60/60)} hours', className="text-sm text-gray-700")
-                ],
-                className="flex-1 p-4"
-            ),
-            html.Div(
-                children=[
-                    html.Div(children="Longest Ride", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f"{"%.2f"%(calculate_longest_ride_for_year(activities)/60/60)} hours", className="text-sm text-gray-700")
-                ],
-                className="flex-1 p-4"
-            ),
-            html.Div(
-                children=[
-                    html.Div(children="Biggest Ride", className="text-lg font-semibold mb-2 text-red-700"),
-                    html.Div(children=f"{"%.2f"%(calculate_biggest_ride(activities)/1000)} KM", className="text-sm text-gray-700")
-                ],
-                className="flex-1 p-4"
-            ),
+            html.H2("Distance (KM) by Date", className="text-lg font-semibold text-center text-red-700 mb-4"),
+            dcc.Graph(id='km-per-day-over-year-graph', figure=generate_km_per_day_over_year_heatmap(activities)),
         ],
-        className="grid grid-cols-3 gap-4 max-w-4xl mx-auto"
+        className="max-w-7xl mx-auto px-4 mb-8 rounded shadow"
     ),
     html.Div(
+        className="grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-7xl mx-auto px-4 mb-12",
         children=[
-            html.H2(children="Distance (KM) by date", className="font-bold justify-self-center text-center w-100 mx-auto text-slate-700"),
-            dcc.Graph(
-                id='km-per-day-over-year-graph',
-                figure=generate_km_per_day_over_year_heatmap(activities),
+            html.Div(
+                className="grid grid-cols-1 md:grid-cols-2 gap-4",
+                children=[
+                    html.Div([
+                        html.Div("Distance", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f'{calculate_total_distance(activities):.2f} KM', className="text-sm text-gray-700"),
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                    html.Div([
+                        html.Div("Streak ⚡", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f"{calculate_streak(activities)} days", className="text-sm text-gray-700"),
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                    html.Div([
+                        html.Div("Rides", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f'{calculate_num_rides(activities)}', className="text-sm text-gray-700")
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                    html.Div([
+                        html.Div("Duration", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f'{calculate_moving_time(activities)/60/60:.2f} hours', className="text-sm text-gray-700")
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                    html.Div([
+                        html.Div("Longest Ride", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f"{calculate_longest_ride_for_year(activities)/60/60:.2f} hours", className="text-sm text-gray-700")
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                    html.Div([
+                        html.Div("Biggest Ride", className="text-lg font-semibold mb-2 text-red-700"),
+                        html.Div(f"{calculate_biggest_ride(activities)/1000:.2f} KM", className="text-sm text-gray-700")
+                    ], className="p-4 shadow rounded flex flex-col justify-center text-center"),
+                ]
             ),
+
+            html.Div(
+                children=[
+                    html.H2("Ride Length", className="text-lg font-semibold text-center text-red-700 mb-4"),
+                    dcc.Graph(
+                        id="ride-length-binned-over-year-graph",
+                        figure=generate_ride_length_binned_over_year_plot(activities),
+                        style={"height": "300px"}
+                    )
+                ],
+                className="bg-white p-4 rounded shadow overflow-hidden"
+            )
         ]
-    ),
-    html.Div(
-        children=[
-            html.H2(children="Ride length", className="font-bold justify-self-center text-center w-100 mx-auto text-slate-700"),
-            dcc.Graph(id="ride-length-binned-over-year-graph", figure=generate_ride_length_binned_over_year_plot(activities)),
-        ]
-    ),
-]
-)
+    )
+])
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
